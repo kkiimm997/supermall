c13969ea0a069e67fa9a7b86fa663050c3f8f7c1
kkiimm997 <2391316296@qq.com>
2022-09-02 10:15:16 +0800

    22/9/2 测试sourcetree

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -2,7 +2,7 @@
   <div class="nav-bar">
     <div class="left"><slot name="left"></slot></div>
     <div class="center"><slot name="center"></slot></div>
-    <div class="right"><slot name="right"></slot></div>
+    <div class="right"><slot name="right">爱的速递</slot></div>
   </div>
 </template>
 
